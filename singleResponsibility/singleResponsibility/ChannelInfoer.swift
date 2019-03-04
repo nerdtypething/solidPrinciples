eadc77f0921182b6cbe2dcef5315bff9fa587050
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2019-03-03 22:46:07 -0700

    - initial broken out class skeletons

---
+++ singleResponsibility/singleResponsibility/ChannelInfoer.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelInfoer.swift
+//  singleResponsibility
+//
+//  Created by Ruben Hansen-Rojas on 3/3/19.
+//  Copyright © 2019 me. All rights reserved.
+//
+
+import Foundation
+
+class ChannelInfoer
+{
+    func printChannelInfo(channels: [Channel])
+    {
+        
+    }
+}
---
+++ singleResponsibility/singleResponsibility/ChannelManager.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelManager.swift
+//  singleResponsibility
+//
+//  Created by Ruben Hansen-Rojas on 3/3/19.
+//  Copyright © 2019 me. All rights reserved.
+//
+
+import Foundation
+
+class ChannelManager
+{
+    private var _channels: [String: Channel] = [:]
+    
+    func addChannel(name: String, value: Int, description: String)
+    {
+        
+    }
+}
