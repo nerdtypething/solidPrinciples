9d6b8349acae6c33b66aa771f2f433d76a35a011
Ruben Hansen-Rojas <ruben.rojas1979@gmail.com>
2019-03-03 22:56:29 -0700

    - implementing channel manager

--- singleResponsibility/singleResponsibility/ChannelManager.swift
+++ singleResponsibility/singleResponsibility/ChannelManager.swift
@@ -12,8 +12,28 @@ class ChannelManager
 {
     private var _channels: [String: Channel] = [:]
     
+    init(channels: [Channel])
+    {
+        channels.forEach({c in
+            self.addChannel(name: c.name, value: c.value, description: c.description)
+        })
+    }
+    
+    func getChannels() -> [Channel]
+    {
+        return Array(_channels.values)
+    }
+    
     func addChannel(name: String, value: Int, description: String)
     {
+        let channelExists = _channels.contains(where: { (key, value) -> Bool in
+            return key == name
+        })
         
+        if (!channelExists)
+        {
+            let newChannel = Channel(name: name, value: value, description: description)
+            _channels[name] = newChannel
+        }
     }
 }
