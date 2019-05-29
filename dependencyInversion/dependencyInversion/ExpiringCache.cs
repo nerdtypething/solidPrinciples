c12a5ede3d582d304d364fa5f2e68035ae500bc1
Hansen-Rojas, Ruben <Ruben.Hansen-Rojas@charter.com>
2019-05-29 09:03:47 -0600

    - adding GetPayments method to view model.

---
+++ dependencyInversion/dependencyInversion/ExpiringCache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace dependencyInversion
+{
+    public class ExpiringCache<T>
+    {
+        public void StoreValue(string key, T value)
+        {
+            // store it for real
+        }
+
+        public T RetrieveValue(string key)
+        {
+            // different implementation to check if 
+            // cache has expired, etc., etc..
+
+            return default(T);
+        }
+
+        public bool IsExpired(string key)
+        {
+            return true;
+        }
+    }
+}
