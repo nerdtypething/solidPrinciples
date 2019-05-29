4e71f99bb03c1d49a8eb40d8ae950d6c2201ad54
Hansen-Rojas, Ruben <Ruben.Hansen-Rojas@charter.com>
2019-05-29 09:09:26 -0600

    - adding IMakePaymentViewModel

---
+++ dependencyInversion/dependencyInversion/DI/IMakePaymentViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace dependencyInversion
+{
+    public interface IMakePaymentViewModel
+    {
+        void SubmitPayment(decimal amount, string mopToken);
+        Payment[] GetPayments();
+    }
+}
