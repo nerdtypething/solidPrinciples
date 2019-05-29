2bbb0b608818d4a2d573917f5370a90eb27cd008
Hansen-Rojas, Ruben <Ruben.Hansen-Rojas@charter.com>
2019-05-29 09:12:03 -0600

    - adding additional concrete data client implementation.

---
+++ dependencyInversion/dependencyInversion/BetterPaymentDataClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace dependencyInversion
+{
+    public class BetterPaymentDataClient
+    {
+        private InMemoryCache<Payment[]> _cache;
+
+        public BetterPaymentDataClient(InMemoryCache<Payment[]> cache)
+        {
+            _cache = cache;
+        }
+
+        public Payment[] GetPayments(DateTime dateRange)
+        {
+            // do fancy logic determining where in the cache
+            // the date range should live, etc., etc..
+            return _cache.RetrieveValue("Payments");
+        }
+
+        public void PostPayment(decimal amount, string mopToken, bool recurring)
+        {
+            // do better work.
+        }
+    }
+}
